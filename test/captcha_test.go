131953cef8c510245dae1f95922dfe55a4941692
weitienwong <weitienwong@outlook.com>
2025-07-03 14:54:28 +0800

    删除凭证数据

--- test/captcha_test.go
+++ test/captcha_test.go
@@ -8,9 +8,9 @@ import (
 )
 
 const (
-	AccessKeyID     = "LTAI5t7c9pCXTnoruV2cSk9S"
-	AccessKeySecret = "LlXw55fqPpL14ZNTBExw9wLwrYddPY"
-	SceneId         = "177jxdo1"
+	AccessKeyID     = "xxx"
+	AccessKeySecret = "xxx"
+	SceneId         = "xxx"
 	Param           = `{"sceneId":"177jxdo1","certifyId":"xxxxxx","deviceToken":"xxxxxxx==","data":"xxxxxx==","..."}`
 )
 
